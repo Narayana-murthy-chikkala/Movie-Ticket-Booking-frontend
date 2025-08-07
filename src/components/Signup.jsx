07aa2d91e0e553d255fcc713e007743486d59d5f
Narayana-murthy-chikkala <narayanamurthychikkala6@gmail.com>
2025-08-07 18:48:44 +0530

    Update Signup.jsx

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -17,7 +17,7 @@ import React, { useState } from 'react';
     }
 
     try {
-      const response = await fetch('http://localhost:3000/api/signup', {
+      const response = await fetch('https://movie-ticket-booking-system-9hwv.vercel.app/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
