1ddbdfe6e5756d27846e025dd7e6b04f699953ea
Narayana-murthy-chikkala <narayanamurthychikkala6@gmail.com>
2025-08-07 18:49:56 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = ({ setIsLoggedIn }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch('https://movie-ticket-booking-system-9hwv.vercel.app/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
